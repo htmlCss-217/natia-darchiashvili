e30179b4fa258e707b72e292dfc6907d00219fa6
natia-darchiashvili <darchiashvilinati@gmail.com>
2024-11-10 22:56:43 +0400

    lesson13 finished

---
+++ script.js
@@ -0,0 +1,18 @@
+// @ts-check
+'use strict';
+
+
+function myFunction () {
+    let celsius = prompt("Enter temperature in Celsius: ");
+    let fahrenheit = (celsius * 9/5) + 32;
+    alert(`The temperature in Fahrenheit is ${fahrenheit.toFixed(2)}`);
+}
+
+function Function() {
+    document.getElementById("Fahrenheit").innerHTML = "The symbol for degree Fahrenheit is °F.";
+  }
+
+  function Function() {
+    document.getElementById("Celsius").innerHTML = "The symbol for degree Celsius is °C.";
+  }  
+
